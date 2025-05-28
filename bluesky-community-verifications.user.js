59a766a96d9e345ac8c5dc783e54701586babf8b
Kieran Klukas <me@dunkirk.sh>
2025-05-28 11:53:50 -0400

    feat: add meta for auto updates

--- bluesky-community-verifications.user.js
+++ bluesky-community-verifications.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Bluesky Community Verifications
 // @namespace    https://tangled.sh/@dunkirk.sh/serif/verifications
-// @version      0.2
+// @version      0.2.1
 // @description  Shows verification badges from trusted community members on Bluesky
 // @author       Kieran Klukas
 // @match        https://bsky.app/*
 // @grant        none
 // @run-at       document-end
+// @updateURL    https://tangled.sh/@dunkirk.sh/serif/raw/main/bluesky-community-verifications.user.meta.js
+// @downloadURL  https://tangled.sh/@dunkirk.sh/serif/raw/main/bluesky-community-verifications.user.js
 // ==/UserScript==
 
 (() => {
